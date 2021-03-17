5e8fed19610816b51fa22c9e1eee35cfb0e41ca5
Walter Rezende <walter-zero@hotmail.com>
2021-03-16 22:36:14 -0300

    #test livedata

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,15 +1,40 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.Event
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.not
+import org.hamcrest.Matchers.nullValue
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
 
+    //run tests synchronously for livedata testing
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
     @Test
     fun addNewTask_setsNewTaskEvent() {
         val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+
+        val observer = Observer<Event<Unit>> {}
+
+        try {
+            tasksViewModel.newTaskEvent.observeForever(observer)
+
+            tasksViewModel.addNewTask()
+
+            val value = tasksViewModel.newTaskEvent.value
+            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
+
+        } finally {
+            tasksViewModel.newTaskEvent.removeObserver(observer)
+        }
     }
 }
